32eb1e744103ae53d5f489138431aaa3c142e622
haolk <haolk@vng.com.vn>
2022-04-11 20:59:20 +0700

    inform

--- src/stories/Inform.stories.tsx
+++ src/stories/Inform.stories.tsx
@@ -4,31 +4,31 @@ import { Inform } from 'components';
 import { Notify } from 'icons';
 
 export default {
-    title: 'Components/Inform',
-    component: Inform,
-    argTypes: {
-        icon: {
-            options: ['None', 'Icon'],
-            mapping: {
-              None: undefined,
-              Icon: <Notify/>,
-            },
-            control: { type: 'select' },
-            defaultValue: 'Icon'
-          },
+  title: 'Components/Inform',
+  component: Inform,
+  argTypes: {
+    severity: {
+      options: ['success', 'info', 'warning', 'error'],
+      control: { type: 'select' },
+      defaultValue: 'info'
     },
+    icon: {
+      options: ['None', 'Icon'],
+      mapping: {
+        None: undefined,
+        Icon: <Notify />,
+      },
+      control: { type: 'select' },
+      defaultValue: 'Icon'
+    },
+  },
 } as ComponentMeta<typeof Inform>;
 
 const Template: ComponentStory<typeof Inform> = (args) => <Inform {...args} />
 export const Base = Template.bind({});
 Base.args = {
-    severity: 'info',
-    icon: <Notify />,
-    content: 'Informative inform with dismiss button',
-};
-
-export const Closeable = Template.bind({});
-Closeable.args = {
-  ...Base.args,
+  severity: 'info',
+  icon: <Notify />,
+  content: 'Informative inform with dismiss button',
   closeable: true,
 };
